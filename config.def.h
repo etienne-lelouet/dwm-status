b103ae6040160cd4d56753488a5450451cfccc43
etienne-lelouet <etiennelelouet@outlook.com>
2022-01-23 22:04:45 +0100

    included types.h in config.def.h to remove some annoying IDE errors, and removed useless string terminators

--- config.def.h
+++ config.def.h
@@ -16,18 +16,20 @@ static const char *datetimefmt[] = {
 };
 
 static const char *batteryargs[] = {
-	"/sys/class/power_supply/BAT0/capacity\0", "/sys/class/power_supply/BAT0/status\0", NULL
+	"/sys/class/power_supply/BAT0/capacity", "/sys/class/power_supply/BAT0/status", NULL
 };
 
 static const char *volumeargs[] = {
-	"/tmp/pactl-volumectl-sink", "pactl-getvolume m\0", NULL
+	"/tmp/pactl-volumectl-sink", "pactl-getvolume m", NULL
 };
 
 // Pour plus tard : la commande qui permet de récupérer toutes les connexions actives non externes :
 // nmcli | sed -nE '/\(externally\)/,/^$/d;s/^(\S*?):\s+?connected.*$/\1/p'
 
-struct module modules[] = {
-	{ .name = "date\0",		.args = { .v = datetimefmt },								.loop_func = getdatetime, .init_func = NULL,	.active = 1 },
-	{ .name = "volume\0",	.args = { .i = AUDIOBACKEND_PULSE, .v = volumeargs },		.loop_func = getvolume,	.init_func = init_pactl_volumectl, .active = 1 },
-	{ .name = "battery\0",	.args = { .v = batteryargs },								.loop_func = getbattery, .init_func = NULL,	.active = 1 }
+#include "types.h"
+
+module modules[] = {
+	{ .name = "date",		.args = { .v = datetimefmt },								.loop_func = getdatetime,	.init_func = NULL,					.active = 1,  },
+	{ .name = "volume",		.args = { .i = AUDIOBACKEND_PULSE, .v = volumeargs },		.loop_func = getvolume,		.init_func = init_pactl_volumectl,	.active = 1 },
+	{ .name = "battery",	.args = { .v = batteryargs },								.loop_func = getbattery,	.init_func = NULL,					.active = 1 }
 };
